812175546b74b24c15f65058da4adeeab3b4214c
VinniFlima <vinicius.fernandes.vfl19@gmail.com>
2026-06-18 01:53:54 +0000

    finalizando programa

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,29 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-
-    int tabuleiro[10][10] = {0};
-
-    printf("=== TABULEIRO BATALHA NAVAL ===\n");
-
-    //navio horizontal na linha 1
-    tabuleiro[1][1] = 3, tabuleiro[1][2] = 3, tabuleiro[1][3] = 3;
-
-    //navio vertical na coluna 5
-    tabuleiro[1][5] = 3, tabuleiro[2][5] = 3, tabuleiro[3][5] = 3;
-
-    //Exibição do tabuleiro
-    for(int i = 0; i < 10; i++) {
-        printf("%d ", i+1);
-        for(int j = 0; j < 10; j++) {
-            printf(" %d", tabuleiro[i][j]);
-        }
-        printf("\n");
-    }
-
-    return 0;
-}
-#include <stdio.h>
+#define LINHA 5
+#define COLUNA 5
 
 int main()
 {
@@ -81,6 +60,108 @@ int main()
         erro_validacao = 1;
     }
 
+    int cruz[LINHA][COLUNA];
+    for (int i = 0; i < LINHA; i++)
+    {
+        for (int j = 0; j < COLUNA; j++)
+        {
+            if (i == 2 || j == 2)
+            {
+                cruz[i][j] = 1;
+            }
+            else
+            {
+                cruz[i][j] = 0;
+            }
+        }
+    }
+    int origem_linha = 4;
+    int origem_coluna = 4;
+
+    for (int i = 0; i < LINHA; i++)
+    {
+        for (int j = 0; j < COLUNA; j++)
+        {
+            int linha_alvo = origem_linha + (i - 2);
+            int coluna_alvo = origem_coluna + (j - 2);
+            if (linha_alvo >= 0 && linha_alvo < 10 && coluna_alvo >= 0 && coluna_alvo < 10)
+            {
+                if (cruz[i][j] == 1)
+                {
+                    tabuleiro[linha_alvo][coluna_alvo] = 5;
+                }
+            }
+        }
+    }
+
+    int cone[LINHA][COLUNA];
+    for (int i = 0; i < LINHA; i++)
+    {
+        for (int j = 0; j < COLUNA; j++)
+        {
+            if (i <= 2 && abs(j - 2) <= i)
+            {
+                cone[i][j] = 1;
+            }
+            else
+            {
+                cone[i][j] = 0;
+            }
+        }
+    }
+    origem_linha = 2;
+    origem_coluna = 4;
+
+    for (int i = 0; i < LINHA; i++)
+    {
+        for (int j = 0; j < COLUNA; j++)
+        {
+            int linha_alvo = origem_linha + (i - 2);
+            int coluna_alvo = origem_coluna + (j - 2);
+            if (linha_alvo >= 0 && linha_alvo < 10 && coluna_alvo >= 0 && coluna_alvo < 10)
+            {
+                if (cone[i][j] == 1)
+                {
+                    tabuleiro[linha_alvo][coluna_alvo] = 5;
+                }
+            }
+        }
+    }
+
+    int oct[LINHA][COLUNA];
+    for (int i = 0; i < LINHA; i++)
+    {
+        for (int j = 0; j < COLUNA; j++)
+        {
+            if (abs(i - 2) + abs(j - 2) <= 2)
+            {
+                oct[i][j] = 1;
+            }
+            else
+            {
+                oct[i][j] = 0;
+            }
+        }
+    }
+    origem_linha = 7;
+    origem_coluna = 7;
+
+    for (int i = 0; i < LINHA; i++)
+    {
+        for (int j = 0; j < COLUNA; j++)
+        {
+            int linha_alvo = origem_linha + (i - 2);
+            int coluna_alvo = origem_coluna + (j - 2);
+            if (linha_alvo >= 0 && linha_alvo < 10 && coluna_alvo >= 0 && coluna_alvo < 10)
+            {
+                if (oct[i][j] == 1)
+                {
+                    tabuleiro[linha_alvo][coluna_alvo] = 5;
+                }
+            }
+        }
+    }
+
     if (erro_validacao == 1)
     {
         printf("Erro na validação de limites ou sobreposição de navios!\n");
@@ -93,7 +174,18 @@ int main()
             printf("%2d ", i + 1);
             for (int j = 0; j < 10; j++)
             {
-                printf(" %d", tabuleiro[i][j]);
+                if (tabuleiro[i][j] == 0)
+                {
+                    printf(" \x1b[34m%d\x1b[0m", tabuleiro[i][j]);
+                }
+                else if (tabuleiro[i][j] == 3)
+                {
+                    printf(" \x1b[33m%d\x1b[0m", tabuleiro[i][j]);
+                }
+                else if (tabuleiro[i][j] == 5)
+                {
+                    printf(" \x1b[31m%d\x1b[0m", tabuleiro[i][j]);
+                }
             }
             printf("\n");
         }
